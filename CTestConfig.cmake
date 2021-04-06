d8e5efad6a30bc418ef103af54f902736ac9e59d
Charles Gueunet <charles.gueunet@gmail.com>
2021-04-06 19:11:38 +0200

    Add CDash

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "Kakoune")
+set(CTEST_NIGHTLY_START_TIME "02:15:00 EST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Kakoune")
+set(CTEST_DROP_SITE_CDASH TRUE)
